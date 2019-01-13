6a01e868d1f66e018512de70fbcfd50cc5bface1
loyinglin <loying@foxmail.com>
2019-01-13 20:29:20 +0800

    demo

---
+++ Demo2_跳一跳/LearnSceneKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnSceneKit
+//
+//  Created by loyinglin on 2018/12/31.
+//  Copyright © 2018 ByteDance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo2_跳一跳/LearnSceneKit/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  LearnSceneKit
+//
+//  Created by loyinglin on 2018/12/31.
+//  Copyright © 2018 ByteDance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
